735ce3bd995876640173692d1b044bbecb2dbc3d
albihasani94 <albihasani94@gmail.com>
2020-01-03 01:31:38 +0100

    Add observable weather

---
+++ observer/src/observer/observable/CurrentConditionsDisplay.java
@@ -0,0 +1,31 @@
+package observer.observable;
+
+import observer.simple.DisplayElement;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    Observable observable;
+    private float temperature;
+    private float humidity;
+
+    public CurrentConditionsDisplay(Observable observable) {
+        this.observable = observable;
+        observable.addObserver(this);
+    }
+
+    public void update(Observable observable, Object arg) {
+        if (observable instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData) observable;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
+            display();
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("Current conditions: temperature %.2f F degrees and %.2f %% humidity\n", temperature, humidity);
+    }
+}
